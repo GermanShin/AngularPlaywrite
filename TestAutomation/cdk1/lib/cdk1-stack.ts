95d7036b124516cb0270aa588301463a91103988
David Shin <germanshin1217@gmail.com>
2025-09-03 11:33:28 +0800

    Remove cdk resource to make it initialized

--- TestAutomation/cdk1/lib/cdk1-stack.ts
+++ TestAutomation/cdk1/lib/cdk1-stack.ts
@@ -5,12 +5,5 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 export class Cdk1Stack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-
-        // Example resource: simple S3 bucket
-        new s3.Bucket(this, 'MyTestBucket', {
-            versioned: true,
-            removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for testing!
-            autoDeleteObjects: true,
-        });
     }
 }
