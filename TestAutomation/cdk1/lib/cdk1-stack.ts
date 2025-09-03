c0175404085e9fd7975a7dcdc01f7db9d7e52eaa
David Shin <germanshin1217@gmail.com>
2025-09-03 15:27:22 +0800

    Create new bucket to save a playwright report

--- TestAutomation/cdk1/lib/cdk1-stack.ts
+++ TestAutomation/cdk1/lib/cdk1-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
 
-export class Cdk1Stack extends cdk.Stack {
+export class PlaywrightReportsStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        // 1. Create S3 bucket for reports (only the first time)
+        const artifactBucket = new s3.Bucket(this, 'PlaywrightReportsBucket', {
+            removalPolicy: cdk.RemovalPolicy.RETAIN, // Keep bucket after stack deletion
+            autoDeleteObjects: false, // Avoid deleting reports accidentally
+        });
     }
 }
