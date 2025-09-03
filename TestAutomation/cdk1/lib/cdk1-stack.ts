dbbb420cbf01d6714f40200a72e838c577173c66
David Shin <germanshin1217@gmail.com>
2025-09-03 15:30:48 +0800

    Fix error on cdk class name

--- TestAutomation/cdk1/lib/cdk1-stack.ts
+++ TestAutomation/cdk1/lib/cdk1-stack.ts
@@ -3,7 +3,7 @@ import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
-export class PlaywrightReportsStack extends cdk.Stack {
+export class Cdk1Stack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
